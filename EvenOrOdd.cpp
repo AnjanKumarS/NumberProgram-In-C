191b0784a09986aedd642dc2cfb35adde2e220f9
AnjanKumarS <anjankumars65432@gmail.com>
2025-01-08 17:24:23 +0530

    Even or Odd

---
+++ EvenOrOdd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 12;
+    if(n/2+n/2==n){
+        cout << "Even" << endl;
+    }
+    else{
+        cout << "Odd" << endl;
+    }
+}
